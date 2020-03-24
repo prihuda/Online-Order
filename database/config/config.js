5958330e7365498d3e850f9fc279b92ea1759791
prihuda <rqul22@gmail.com>
2020-03-24 18:15:05 +0700

    commit kesekian kalinya

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL || 'postgres://tyqlebwhrrbtbg:ad3190a04aee8be30b0f78d334a2def94f617b5c2b35cedb49cb24c63a5834d3@ec2-35-174-88-65.compute-1.amazonaws.com:5432/d5ufji4hd781d2',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
